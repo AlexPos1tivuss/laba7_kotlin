8d1baa253e425e3742f2fe54086d6dd32da842f0
Sasha Borisov <sb.2004.bor@gmail.com>
2024-02-15 19:15:00 +0300

    Update NotesScreen.kt

--- app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
@@ -1,23 +1,23 @@
 package com.topic2.android.notes.ui.screens
-
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.topic2.android.notes.viewmodel.MainViewModel
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
+import com.topic2.android.notes.ui.components.TopAppBar
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
 import com.topic2.android.notes.domain.model.NoteModel
 import com.topic2.android.notes.ui.components.Note
-import com.topic2.android.notes.ui.components.TopAppBar
-import com.topic2.android.notes.viewmodel.MainViewModel
-import androidx.compose.runtime.getValue
+import android.annotation.SuppressLint
+import androidx.compose.material.*
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.rememberCoroutineScope
-
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.ui.components.AppDrawer
+import kotlinx.coroutines.launch
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun NotesScreen(
@@ -28,12 +28,38 @@ fun NotesScreen(
         .observeAsState(listOf())
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val coroutineScope = rememberCoroutineScope()
-
-    Scaffold (
-        topBar = {
-        TopAppBar(title = "Заметки",
+    Scaffold (topBar = {
+        TopAppBar(
+            title = "Notes",
             icon = Icons.Filled.List,
-            onIconClick = {}
+            onIconClick = {
+                coroutineScope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }
+        )
+    },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            AppDrawer(
+                currentScreen = Screen.Notes, closeDrawerAction = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                }
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { viewModel.onCreateNewNoteClick() },
+                contentColor = MaterialTheme.colors.background,
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add Note Button"
+                    )
+                }
             )
         },
         content = {
@@ -48,7 +74,6 @@ fun NotesScreen(
         }
     )
 }
-
 @Composable
 private fun NotesList(
     notes: List<NoteModel>,
@@ -65,7 +90,6 @@ private fun NotesList(
         }
     }
 }
-
 @Preview
 @Composable
 private fun  NotesListPreview(){
